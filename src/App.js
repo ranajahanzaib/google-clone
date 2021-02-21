e2db276092875a48fac6e5e2c4cda58a57bb6f41
ranajahanzaib <iam@ranajahanzaib.com>
2021-02-22 03:45:08 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import SearchPage from "./Pages/SearchPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/search" exact component={SearchPage} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
